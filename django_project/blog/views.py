b71cc60c6b31c4ce2ae3ff78af0acc70a62d979e
Madelene Campos <madelenecampos@gmail.com>
2021-02-05 10:49:27 -0500

    Adds context dummy data to home page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Maddie',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 5, 2021'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 6, 2021'
+    },
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
