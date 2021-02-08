b20eb50931967fef3427eb1ff2748cdd691e4ffc
Madelene Campos <madelenecampos@gmail.com>
2021-02-08 15:57:06 -0500

    Grabs data from the database and uses django template filter

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'Maddie',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'February 5, 2021'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'February 6, 2021'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
