62168ab10ceb4c17d100c04ff4b8d2cb8756a788
Madelene Campos <madelenecampos@gmail.com>
2021-02-15 14:46:20 -0500

    Adds mixin so that a user who is logged out can not add a new post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView
 
@@ -15,7 +16,7 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
