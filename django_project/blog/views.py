02cde05ba35eeb95eb7d4bf9ea5b608810420e13
Madelene Campos <madelenecampos@gmail.com>
2021-02-15 16:25:50 -0500

    Adds Update and ensures only the author can update their own post and no one else's

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Post
 
@@ -24,6 +24,23 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         # sets author to current logged in user
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # sets author to current logged in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        # prevents users from updating other's posts
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
