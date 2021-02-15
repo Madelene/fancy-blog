e63f9495cc9cd8777ced2a0bb2df6ccce802f802
Madelene Campos <madelenecampos@gmail.com>
2021-02-15 14:43:57 -0500

    Adds create view and returns url as a string so the view may redirect. Ability to create a new blog post, here

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import PostDetailView, PostListView
+from .views import PostCreateView, PostDetailView, PostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Post
 
@@ -13,6 +13,16 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # sets author to current logged in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
